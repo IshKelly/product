d0c8de91f4d303582e5fcae4937b0ed6456da414
IshKelly <1967267@cegepoutaouais.qc.ca>
2022-09-01 12:04:02 -0400

    3 files modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
+
 export class CartComponent {
+
+  constructor(
+    private cartService: CartService
+  ) { }
 
 }
